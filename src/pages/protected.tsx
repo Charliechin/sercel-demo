dbf7acc11ab9ac6e9622843f5a2f44028a382d9f
Carlos Diaz <charliedn@gmail.com>
2023-02-03 18:35:17 +0000

    Add protected route to the project

---
+++ src/pages/protected.tsx
@@ -0,0 +1,36 @@
+import { withSSRContext } from "aws-amplify"
+
+// TODO: types
+const Protected = ({ authenticated, username }: any) => {
+  // if (!authenticated) return <h1>Not Authenticated</h1>
+  // return <h1>Hello {username} from SSR route</h1>
+  return (
+    <h1>{authenticated ? `Hello ${username} from SSR route` : 'Not auth'}</h1>
+  )
+}
+
+// TODO: types
+export const getServerSideProps = async (ctx: any) => {
+  const { Auth } = withSSRContext(ctx)
+  try {
+    const user = await Auth.currentAuthenticatedUser()
+    return {
+      props: {
+        authenticated: true,
+        username: user.username
+      }
+    }
+
+  } catch (error) {
+    console.log(error)
+    return {
+      props: {
+        authenticated: false
+      }
+    }
+
+  }
+}
+
+
+export default Protected
