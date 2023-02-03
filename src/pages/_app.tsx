30c74246a828ff7657736079fbcb3b4903035304
Carlos Diaz <charliedn@gmail.com>
2023-02-03 18:24:28 +0000

    Configure Amplify to the root of our app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Amplify } from 'aws-amplify'
+import awsconfig from '../../aws-exports'
+
+Amplify.configure({ ...awsconfig, ssr: true })
 
 export default function App({ Component, pageProps }: AppProps) {
   return <ChakraProvider>
