f59d68634e61a84c8d290b96c9aa977f15570cb7
Carlos Diaz <charliedn@gmail.com>
2023-02-06 17:22:10 +0000

    wrap context provider around App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,26 @@
 // Props
 import type { AppProps } from 'next/app'
 // Amplify
-import { Amplify } from 'aws-amplify'
+import { Amplify, Auth } from 'aws-amplify'
 import awsExports from '../../aws-exports'
 // Styles
 import '@aws-amplify/ui-react/styles.css'
 import "../styles/globals.css"
 import { ChakraProvider } from '@chakra-ui/react'
+import Header from '@/components/Header'
+import AuthProvider from '@/contexts/Auth'
 
 Amplify.configure({ ...awsExports, ssr: true })
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider>
+        <Header />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider>
+
   )
 }
