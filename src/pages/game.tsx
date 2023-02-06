ce7163cd3ee3c2ccb66b165eb94eb4f9d7ad9987
Carlos Diaz <charliedn@gmail.com>
2023-02-06 21:44:57 +0000

    Created game file

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -1,7 +1,78 @@
-const Game = () => {
+import React from "react";
+import { useState } from "react";
+import { Box, Flex, Heading, Text, Image, Button } from "@chakra-ui/react";
 
-  return (<p>Game</p>)
-
+interface IProduct {
+  id: number
+  name: string
+  description: string
+  image: string
+  price: number
 }
 
-export default Game
+const products: IProduct[] = [
+  {
+    id: 1,
+    name: "Product 1",
+    description: "This is product 1",
+    image: "/product1.jpg",
+    price: 9.99
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    description: "This is product 2",
+    image: "/product2.jpg",
+    price: 19.99
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    description: "This is product 3",
+    image: "/product3.jpg",
+    price: 29.99
+  }
+];
+
+const Game = () => {
+  const [cart, setCart] = useState<IProduct[] | []>([]);
+
+  const handleAddToCart = (product: IProduct) => {
+    setCart([...cart, product]);
+  };
+
+  return (
+    <Box p={4}>
+      <Heading as="h1" size="lg">
+        Our Products
+      </Heading>
+      <Flex flexWrap="wrap">
+        {products.map(product => (
+          <Box key={product.id} p={4} width={[1 / 2, 1 / 3, 1 / 4]}>
+            <Image src={product.image} />
+            <Heading as="h2" size="md">
+              {product.name}
+            </Heading>
+            <Text>{product.description}</Text>
+            <Text>${product.price}</Text>
+            <Button onClick={() => handleAddToCart(product)}>
+              Add to Cart
+            </Button>
+          </Box>
+        ))}
+      </Flex>
+      <Heading as="h2" size="lg">
+        Cart
+      </Heading>
+      <ul>
+        {cart.map(product => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </Box>
+  );
+};
+
+export default Game;
