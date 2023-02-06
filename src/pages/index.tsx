ea5b05d1730f4605382270775473664f8545e753
Carlos Diaz <charliedn@gmail.com>
2023-02-06 17:58:56 +0000

    remove fetching custom font from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,79 +14,70 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <Container maxW={'3xl'}>
-        <Stack
-          as={Box}
-          textAlign={'center'}
-          spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}>
-          <Heading
-            fontWeight={600}
-            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
-            lineHeight={'110%'}>
-            Speedy
-            <Text as={'span'} color={'green.400'}>
-              Spend
-            </Text>
-          </Heading>
-          <Text color={'gray.500'}>
-            Choose which billionaire you would like to be and try to spend your fortune as quickly as possible.
+    <Container maxW={'3xl'}>
+      <Stack
+        as={Box}
+        textAlign={'center'}
+        spacing={{ base: 8, md: 14 }}
+        py={{ base: 20, md: 36 }}>
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+          lineHeight={'110%'}>
+          Speedy
+          <Text as={'span'} color={'green.400'}>
+            Spend
           </Text>
-          <Stack
-            direction={'column'}
-            spacing={3}
-            align={'center'}
-            alignSelf={'center'}
-            position={'relative'}>
-            <Link href="/game">
-              <Button
-                colorScheme={'green'}
-                bg={'green.400'}
-                rounded={'full'}
-                px={6}
-                _hover={{
-                  bg: 'green.500',
-                }}>
-                Get Started
-              </Button>
-            </Link>
-            <Link href="/rules">
-              <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
-                Rules
-              </Button>
-            </Link>
-            <Box>
-              <Icon
-                as={Arrow}
-                color={useColorModeValue('gray.800', 'gray.300')}
-                w={71}
-                position={'absolute'}
-                right={-71}
-                top={'10px'}
-              />
-              <Text
-                fontSize={'lg'}
-                fontFamily={'Caveat'}
-                position={'absolute'}
-                right={'-125px'}
-                top={'-15px'}
-                transform={'rotate(10deg)'}>
-                We'll give you a minute!
-              </Text>
-            </Box>
-          </Stack>
+        </Heading>
+        <Text color={'gray.500'}>
+          Choose which billionaire you would like to be and try to spend your fortune as quickly as possible.
+        </Text>
+        <Stack
+          direction={'column'}
+          spacing={3}
+          align={'center'}
+          alignSelf={'center'}
+          position={'relative'}>
+          <Link href="/game">
+            <Button
+              colorScheme={'green'}
+              bg={'green.400'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'green.500',
+              }}>
+              Get Started
+            </Button>
+          </Link>
+          <Link href="/rules">
+            <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
+              Rules
+            </Button>
+          </Link>
+          <Box>
+            <Icon
+              as={Arrow}
+              color={useColorModeValue('gray.800', 'gray.300')}
+              w={71}
+              position={'absolute'}
+              right={-71}
+              top={'10px'}
+            />
+            <Text
+              fontSize={'lg'}
+              fontFamily={'Caveat'}
+              position={'absolute'}
+              right={'-125px'}
+              top={'-15px'}
+              transform={'rotate(10deg)'}>
+              You only have one minute!
+            </Text>
+          </Box>
         </Stack>
-      </Container>
-    </>
-  );
+      </Stack>
+    </Container>
+  )
 }
 
 const Arrow = createIcon({
